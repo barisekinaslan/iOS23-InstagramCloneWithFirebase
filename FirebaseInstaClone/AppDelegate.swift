84f1dfe29b222dc840e9e05a900a5c090aab5234
Barış Ekin Aslan <barisekin3562@gmail.com>
2025-10-31 12:57:48 +0300

    Initial commit

--- FirebaseInstaClone/AppDelegate.swift
+++ FirebaseInstaClone/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+import FirebaseStorage
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
---
+++ FirebaseInstaClone/SettingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsViewController.swift
+//  FirebaseInstaClone
+//
+//  Created by Barış Ekin Aslan on 22.08.2025.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        } catch {
+            print("error")
+        }
+       
+    }
+    
+
+}
